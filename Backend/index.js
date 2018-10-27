22b6608e9ce73fb490fb84d106c1afbae64485fe
Teerapat Tee <teerapat12345678@gmail.com>
2018-10-27 16:19:14 +0700

    set-up init environtment backend

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')    //เรียกใช้ mongoose
+
+const PORT = 8000
+// const routes = require('./routes')
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/itservice', {
+     useNewUrlParser: true
+})
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ type: '*/*' }))
+// routes(app)
+
+app.listen(PORT, () => {
+    console.log('ready server on http://localhost:' + PORT)
+})
