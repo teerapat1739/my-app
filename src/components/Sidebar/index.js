1c021c2e04c42b51b869c03b00d33643eec56cd1
Teerapat Tee <teerapat12345678@gmail.com>
2018-10-27 10:14:23 +0700

    split some style sidebar(.css)  to style-component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react'
 import './index.css'
+import styled from "styled-components";
+
+
+const HeaderNavbar = styled.div`
+    display: block;
+    margin: 0 auto;
+    width: 100%;
+    max-width: 100%;
+    box-shadow: none;
+    background-color: #FC466B;
+    position: fixed;
+    height: 60px!important;
+`
+
+const Content = styled.div`
+    margin: 0 auto;
+    display: block;
+    height: 100%;
+    margin-top: 60px;
+`
+
+
 class SelectLanguage extends Component {
     render() {
         return (
             <div>
-                <div class="header"></div>
-            <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu" />
-            <label for="openSidebarMenu" class="sidebarIconToggle">
-                <div class="spinner diagonal part-1"></div>
-                <div class="spinner horizontal"></div>
-                <div class="spinner diagonal part-2"></div>
-            </label>
-            <div id="sidebarMenu">
-                <ul class="sidebarMenuInner">
-                <li>General</li>
-                <li>Security</li>
-                </ul>
-            </div>
-            <div id='center' class="main center">
-                <div>{this.props.children}</div>
-            </div>
+                <HeaderNavbar>
+                    <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu" />
+                    <label for="openSidebarMenu" class="sidebarIconToggle">
+                        <div class="spinner diagonal part-1"></div>
+                        <div class="spinner horizontal"></div>
+                        <div class="spinner diagonal part-2"></div>
+                    </label>
+                    <div id="sidebarMenu">
+                        <ul class="sidebarMenuInner">
+                        <li>General</li>
+                        <li>Security</li>
+                        </ul>
+                    </div>
+                    <Content>
+                        <div>{this.props.children}</div>
+                    </Content>
+                </HeaderNavbar>
             </div>
         )
     }
