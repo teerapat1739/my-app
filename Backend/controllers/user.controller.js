8aa52c3c1acee46c9a000716babcec0057fb96e0
Teerapat Tee <teerapat12345678@gmail.com>
2018-10-28 13:41:30 +0700

    refactoring user.controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,17 +1,20 @@
 const User = require('../models/user.model')
 
 
+//ฟังก์ชันสร้างข้อมูลผู้ใช้ใหม่
 exports.create = (req, res, next) => {
-    console.log('create')
+    console.log(req.body)
+    console.log(req.body.email)
     User.findOne({ email: req.body.email }, (err, result) => {
         if (err) { return next(err); }
 
         if (result) {
-            res.json({ status: 201, message: 'Username is Duplicate' })
+            res.json({ status: 201, message: 'email is Duplicate' })
         } else {
             const user = new User(req.body)
             user.save(err => {
                 if (err) { return next(err) }
+
                 res.json(user)
             })
         }
