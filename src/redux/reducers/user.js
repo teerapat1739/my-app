77408664c8a892a8a15c94ec6e6c76c53c49877b
Teerapat Tee <teerapat12345678@gmail.com>
2018-10-29 20:58:25 +0700

    load user data into each input generpage

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,10 +1,10 @@
 export default (state = {}, action) => {
     switch (action.type) {
       case 'LOAD_USER_SUCCESS':
-        console.log('SAVE_USER_SUCCESS')
+        console.log('LOAD_USER_SUCCESS')
         return { ...state,  data: action.payload }
       case 'LOAD_USER_FAILED':
-        console.log('SAVE_USER_SUCCESS')
+        console.log('LOAD_USER_SUCCESS')
         return { ...state,  data: action.payload }
       default:
         return state;
